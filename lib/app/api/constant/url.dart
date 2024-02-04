8982d64e26a0d1e984fd280294cc9d04a24bedb7
SyahranFD <fadhilrafa1@gmail.com>
2024-02-04 22:47:20 +0700

    feat: [SF] finishing ui home

--- lib/app/api/constant/url.dart
+++ lib/app/api/constant/url.dart
@@ -1,4 +1,4 @@
-String baseUrl = "https://api.themoviedb.org/3/movie/top_rated$apiKey";
+String baseUrl = "https://api.themoviedb.org/3/movie/popular$apiKey";
 String apiKey = "?api_key=5e30e788ba0016174d8c885253084699";
 String imageUrl = "https://image.tmdb.org/t/p/w500";
 
